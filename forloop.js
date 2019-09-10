d40a8c699d6e5ec4a28fadc1cfd0c78596d91872
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-09-10 11:14:50 +0200

    Done return exercise

---
+++ forloop.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/* for (let counter = 0; counter <= 9; counter++) {
+  console.log(`From 0 to 9: ${counter}`);
+}
+
+for (let counter = 1; counter <= 10; counter++) {
+  console.log(`From 1 to 10: ${counter}`);
+} */
+
+/* for (let counter = 10; counter >= 0; counter--) {
+  console.log(`From 10 to 0: ${counter}`);
+  if (counter === 0) console.log("liftoff");
+} */
+
+/* for (let counter = 1; counter <= 19; counter++) {
+  if ((counter % 2) - 1) continue;
+  console.log(`From 1 to 19 and odd: ${counter}`);
+}
+ */
+
+for (let counter = 1; counter <= 16777216; counter += counter) {
+  console.log(`From 1 to 16777216: ${counter}`);
+}
+
+/* for (let counter = 111; counter <= 138; counter++) {
+  if (counter % 3) continue;
+  console.log(`From 111 to 138 in 3%: ${counter}`);
+}
+ */
+
+/* for (let counter = 100; counter >= 0; counter--) {
+  if (counter % 10) continue;
+  console.log(`From 100 to 0 in 10%: ${counter}`);
+}
+ */
